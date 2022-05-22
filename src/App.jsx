ac951a0a5ac298630a5e57cb4e8db8ece956a391
Aminox <aminetrigui384@gmail.com>
2022-05-22 18:05:06 +0200

    2nd

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,46 @@
 import React,{useState, useEffect} from 'react';
+
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Routes
+} from "react-router-dom";
+
 import ListeDePokemon from './ListeDePokemon'
 import './index.css';
 import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import AddPokemon from './pages/AddPokemon/AddPokemon'
+import Pokemons from './pages/Pokemons/Pokemons'
+import DeletePokemon from './pages/DeletePokemon/DeletePokemon'
+import EditPokemon from './pages/EditPokemon/EditPokemon'
+import Home from './pages/Home/Home'
+
 
 
 function App() {
-  const [pokemon, setPokemon]=useState([]);
-  useEffect(() => {
-    axios.get("http://localhost:3001/pokemons").then(res=>{
-    setPokemon(res.data.map(e=>
-      
-      
-      <div>
-        <div>ID: {e.id}</div>
-        <h1>{e.name.french}</h1>
-        <div>Speed: {e.base.Speed}</div>
-            
-        <br></br>
-        <div>
-          
-          <img src={require(`./pokemonimg/${e.id}.png`)} />
-        </div>
-
-
-      </div>
-      
-
-      ))
-      
-  })
-  },[])
-
   return (
-     <ListeDePokemon pokemon={pokemon}/>
+    <Router>
+            <Routes>
+
+                    <Route path="/" element={<Home/>}></Route>
+                    <Route path="/Pokemons" element={<Pokemons />}></Route>
+                    <Route path="/addpokemon" element={<AddPokemon/>}></Route>
+                    <Route path="/DeletePokemon" element={<DeletePokemon/>}></Route>
+                    <Route path="/EditPokemon" element={<EditPokemon/>}></Route>
+                  
+            </Routes>
+
+    </Router>
   );
 }
 
+
+
 export default App;
+
+
 
