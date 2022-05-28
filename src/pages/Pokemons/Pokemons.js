23f26640ad6c157fb779ce11c1536880a60af751
Aminox <aminetrigui384@gmail.com>
2022-05-28 13:52:17 +0200

    2pm

--- src/pages/Pokemons/Pokemons.js
+++ src/pages/Pokemons/Pokemons.js
@@ -1,50 +1,55 @@
-import React, { useState, useEffect } from 'react';
-
-import {
-  BrowserRouter as Router,
-  Route,
-  Link,
-  Routes
-} from "react-router-dom";
-
+import React,{useState, useEffect} from 'react';
 import ListeDePokemon from '../../ListeDePokemon'
 import '../../index.css';
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
-
 function App() {
-  const [pokemon, setPokemon] = useState([]);
 
+  const [pokemon, setPokemon]=useState([]);
+  
   useEffect(() => {
-    axios.get("http://localhost:3001/pokemons").then(res => {
-      setPokemon(res.data.map(e =>
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '50vh' }}>
+    axios.get("http://localhost:3001/pokemons").then(res=>{
+    setPokemon(res.data.map(e=>
+   
+    /*  <div className="pokemon-container">
+        <img src={  e.id <810 ? require(`../../pokemonimg/${e.id}.png`) : (`../../pokemonimg/unknown.png`)}/>
+        <h3 id="ID">ID: {e.id}</h3>
+        <h5 id="Name">Name:{e.name.french}</h5>
+        <h6 id="Base">HP: {e.base.HP}</h6>
+        <h6 id="Attack">Attack: {e.base.Attack}</h6>
+        <h6 id="Speed">Speed: {e.base.Speed}</h6>
+        <h6 id="Defense">Defense: {e.base.Defense}</h6>
+      </div>*/
 
+  <div class="container container-pokemons">
+  <div class="row">
+    <div class="col">
+    <img src={  e.id <810 ? require(`../../pokemonimg/${e.id}.png`) : (`../../pokemonimg/unknown.png`)}/>
+    </div>
+    <div class="col">
+        <h3 id="ID">ID: {e.id}</h3>
+        <h5 id="Name">Name:{e.name.french}</h5>
+        <h6 id="Base">HP: {e.base.HP}</h6>
+        <h6 id="Attack">Attack: {e.base.Attack}</h6>
+        <h6 id="Speed">Speed: {e.base.Speed}</h6>
+        <h6 id="Defense">Defense: {e.base.Defense}</h6>
+    </div>
+  </div>
+  <hr id="hr1"></hr>
+  </div>
 
-          <div>
-            <div>ID: {e.id}</div>
-            <h1>{e.name.french}</h1>
-            <div>Speed: {e.base.Speed}</div>
-
-            <br></br>
-            <div>
-              <img src={require(`../../pokemonimg/${e.id}.png`)} />
-            </div>
-
-
-          </div>
-        </div>
-
-
+      
+      
+      
       ))
-
-    })
-  }, [])
+  })
+  },[])
 
   return (
-    <ListeDePokemon pokemon={pokemon} />
+    <div>
+     <ListeDePokemon pokemon={pokemon}/>
+    </div>
   );
 }
 
